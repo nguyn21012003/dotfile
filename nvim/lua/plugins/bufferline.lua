7e8f245ff6958c93868c5e547a1a38f4a7430bd3
Jeremy Tran <nguyendeptrai980@gmail.com>
2024-12-21 20:25:39 +0700

    update neovim

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,3 +1,19 @@
 return {
-  { "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons" },
+  {
+    "akinsho/bufferline.nvim",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    event = "VeryLazy",
+    keys = {
+      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+    },
+    -- opts = {
+    --   options = {
+    --     mode = "tabs",
+    --     -- separator_style = "slant",
+    --     show_buffer_close_icons = true,
+    --     show_close_icon = true,
+    --   },
+    -- },
+  },
 }
